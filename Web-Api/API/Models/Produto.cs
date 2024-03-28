03652eaa4e811509e5f40ba40b51d3be85786b5c
makzs <erikmakiak@gmail.com>
2024-03-28 10:59:13 -0300

    aprofundamento do GET

---
+++ Web-Api/API/Models/Produto.cs
@@ -0,0 +1,46 @@
+﻿namespace API.Models;
+
+public class Produto
+{
+
+    // construtor
+
+    public Produto()
+    {
+
+    }
+
+    public Produto(string nome, string descricao, double valor)
+    {
+        Nome = nome;
+        Descricao = descricao;
+        Valor = valor;
+    }
+
+    // atributos ou propriedades -> caracteristicas de um objeto
+    // get set em C# (transforma de publico pra privado automaticamente)
+    // o ? é para tratamentos de excecoes, o valor padrao de uma string é nulo e isso pode dar problema
+    public string? Nome { get; set; }
+    public string? Descricao { get; set; }
+
+    public double Valor { get; set; }
+
+
+    // get set em java
+
+    // private string nome;
+    // private string descricao;
+
+    //  guardar informação
+    // public void setNome(string nome)
+    // {
+
+    //     this.nome = nome;
+    // }
+
+    //  retorna a informação
+    // public string getNome()
+    // {
+    //     return this.nome;
+    // }
+}
--- Web-Api/API/Program.cs
+++ Web-Api/API/Program.cs
@@ -1,34 +1,43 @@
+
+using API.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-List<Produto> produtos = new List<Produto>();
-var produto1 = new Produto("Arroz", "pacote de arroz 2kg");
-var produto2 = new Produto("feijao", "pacote de feijao 1kg");
+// Produto produto = new Produto();
+// produto.Nome = "Bolacha";
 
-produtos.Add(produto1);
-produtos.Add(produto2);
+List<Produto> produtos = new List<Produto>();
+produtos.Add(new Produto("Arroz", "pacote de arroz 2kg", 20.50));
+produtos.Add(new Produto("feijao", "pacote de feijao 1kg", 13.20));
+produtos.Add(new Produto("Batata", "saco de 1kg de batata", 15.00));
+produtos.Add(new Produto("Frango", "pacote de 500g de frango", 18.85));
+
+// novo endpoint para a raiz da aplicação
+// GET: http://localhost:5056
+app.MapGet("/", () => "API de produtos");
 
 // o metodo get pega a / como url, "/" é a raiz 
 // é possivel alterar para outra url como "qualquercoisa"
 // ou aterar para uma url produto/listar 
-app.MapGet("/produto/listar", () => {
-    return produtos;
-});
+// GET: http://localhost:5056/produto/listar
+app.MapGet("/produto/listar", () =>
+    produtos);
 
 // nao se pode criar o mesmo metodo e a mesma url se nao ocorre conflito de endpoints
 // app.MapGet("/produto/listar", () => "listagem de produtos");
 
 // metodo post -> cadastro 
 // app.MapPost("/produto/cadastrar", () => "Cadastro de produtos"); -> erro 405. Toda funcao acessada por uma url é pelo metodo get
-app.MapPost("/produto/cadastrar", () => "Cadastro de produtos"); 
+// GET: http://localhost:5056/produto/cadastrar
+app.MapPost("/produto/cadastrar", () => "Cadastro de produtos");
 
 // get é para pegar informações
 // post é para enviar informações
 
 // exercicio
 // tentar cadastar produtos em uma lista
-
+app.MapGet("produto/cadastrar/{id}", (int id) => $"Obter item com ID {id}");
 
 app.Run();
 
-record Produto(string nome, string descricao);
